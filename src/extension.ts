4551d27c3958029a6d6f45c997ff6b12036dacac
Ken Ekeoha <kenomaru@hotmail.it>
2019-05-16 21:24:49 +0200

    0.1.3

--- src/extension.ts
+++ src/extension.ts
@@ -7,19 +7,42 @@ import { TextEditor, window, ExtensionContext, Range, TextEditorSelectionChangeK
 import * as settings from './settings';
 import * as logger from "./logger";
 
-function isTracked(text: string): string | undefined {
 
-    let mappings = settings.Get(settings.Mappings) as Array<Array<string>>;
+function isTracked(text: string, languageId: string): string | undefined {
 
-    if (mappings != null || mappings != undefined) {
-        let count = mappings.length;
-        for (let index = 0; index < count; index++) {
-            const pair = mappings[index];
-            if (pair[0] == text) return pair[1];
+    let MappingBundle: any = settings.Get(settings.Mappings) as object;
 
+    var trueMappings: any | undefined = undefined;
+
+    try {
+
+
+        if (MappingBundle != null && MappingBundle != undefined) {
+            // check if there are language specific mappings.
+            let langSpecificMappings = languageId in MappingBundle ? MappingBundle[languageId] : undefined;
+            
+            if (langSpecificMappings != undefined &&  text in langSpecificMappings) {
+                trueMappings = langSpecificMappings;
+            }
+            else {
+                // if not present in the language specific mapping, try to search in the common mappings.
+                let commonMappings = "common" in MappingBundle ? MappingBundle["common"] : undefined;
+                if (commonMappings != undefined) {
+                    trueMappings = commonMappings;
+                }
+
+            }
         }
-    }
 
+        if (trueMappings === undefined)
+            return undefined;
+
+        var replacer = trueMappings[text];
+        return replacer;
+
+    } catch (error) {
+        logger.SafeLog("Thunder: " + error + "\n. Please check your Thunder settings. Maybe the mappings are not correct?");
+    }
     return undefined;
 }
 // this method is called when your extension is activated
@@ -31,59 +54,59 @@ export function activate(context: ExtensionContext) {
     let _prevT: number | undefined = undefined;
     let _prev_DocVersion: number = -1;
     let _prevString: string | undefined = undefined;
-   
+
 
     let subOnDidChangedTextEditor = window.onDidChangeActiveTextEditor((editor: TextEditor | undefined) => {
 
-        
-            
-                if (editor === undefined) {
-                    logger.SafeLog("Thunder: Text Editor Changed: No Valid Editor.");
-                }
-                else {
-                    logger.SafeLog("Thunder: Text Editor Changed: New Text Editor ready.");
-                }
-               
-                // reset versions
-                _prev_DocVersion = -1;
-                // reset timestamps
-                _prevT = undefined;
-                // reset character
-                _prevString = undefined;
-            
-        
+
+
+        if (editor === undefined) {
+            logger.SafeLog("Thunder: Text Editor Changed: No Valid Editor.");
+        }
+        else {
+            logger.SafeLog("Thunder: Text Editor Changed: New Text Editor ready.");
+        }
+
+        // reset versions
+        _prev_DocVersion = -1;
+        // reset timestamps
+        _prevT = undefined;
+        // reset character
+        _prevString = undefined;
+
+
 
 
     });
 
-    async function TryExecuteReplacement( editor:TextEditor,_curChar:string, _curT: number, curCharRange: Range,  _replacer: string | undefined) : Promise<boolean> {
-        
+    async function TryExecuteReplacement(editor: TextEditor, _curChar: string, _curT: number, curCharRange: Range, _replacer: string | undefined): Promise<boolean> {
+
         let _onTime = (_prevT != null && _prevT != undefined && _curT != undefined &&
             _curT - _prevT < (settings.Get(settings.MaxInterval) as number));
 
-        if (_onTime){
-              // determine the string to be replaced.
-              let replacedRange = new Range(curCharRange.start.translate(0, -1), curCharRange.end);
-              var result = await editor.edit(function (builder) {
-                  // Replace!
-                  builder.replace(replacedRange, _replacer as string);
-                  // the replacement was done so now reset.          
-              });
-              logger.SafeLog("Thunder: Replacement executed successfully for the pair: [" + _curChar + " | " + _replacer + "]");
-              return result;
+        if (_onTime) {
+            // determine the string to be replaced.
+            let replacedRange = new Range(curCharRange.start.translate(0, -1), curCharRange.end);
+            var result = await editor.edit(function (builder) {
+                // Replace!
+                builder.replace(replacedRange, _replacer as string);
+                // the replacement was done so now reset.          
+            });
+            logger.SafeLog("Thunder: Replacement executed successfully for the pair: [" + _curChar + " | " + _replacer + "]");
+            return result;
         }
-        else{
+        else {
             logger.SafeLog("Thunder: Replacement Opportunity not taken for excessive delay.");
             return false;
         }
-       
+
     }
 
     var subOnDidChangeTextEditorSelection = window.onDidChangeTextEditorSelection(async function (event) {
 
         if (event.kind == TextEditorSelectionChangeKind.Keyboard) {
 
-          
+
 
             // timestamp of this text change
             let _curT: number | undefined = new Date().getTime();
@@ -91,8 +114,11 @@ export function activate(context: ExtensionContext) {
             // current version of the document.
             let _cur_DocVersion = event.textEditor.document.version;
 
+            // get the language of the document
+            let _cur_DocLang = event.textEditor.document.languageId;
+
             // an actual text change was made.
-            if (_prev_DocVersion < _cur_DocVersion) {  
+            if (_prev_DocVersion < _cur_DocVersion) {
 
                 // go back to the previous caret position.
                 let backpos = event.selections[0].start.translate(0, -1);
@@ -103,24 +129,24 @@ export function activate(context: ExtensionContext) {
                 // get text in range.
                 let _curString = event.textEditor.document.getText(curCharRange);
 
-                
+
                 // did the user pressed a tracked character?
                 // P.S the isTracked function returns the replacer if text is a tracked string..
-                let _replacer = isTracked(_curString);
+                let _replacer = isTracked(_curString, _cur_DocLang);
 
                 let _isTracked = (_replacer != null);
 
-                let _replaced:boolean = false;
+                let _replaced: boolean = false;
 
-                logger.SafeLog("Thunder: '" +  _curString +  "' was typed."  + (_isTracked ? "it is a tracked character!" : "it is not a tracked character..."));
+                logger.SafeLog("Thunder: '" + _curString + "' was typed." + (_isTracked ? "it is a tracked character!" : "it is not a tracked character..."));
 
                 if (_isTracked && _prevString != undefined && _prevT != undefined && _prevString === _curString) {
-                             // was the tracked character pressed in time?
-                             _replaced=  await TryExecuteReplacement(event.textEditor,_curString, _curT, curCharRange, _replacer);                                                                    
-            
+                    // was the tracked character pressed in time?
+                    _replaced = await TryExecuteReplacement(event.textEditor, _curString, _curT, curCharRange, _replacer);
+
                 }
-               
-                _prevString = _replaced ? _replacer : _curString;              
+
+                _prevString = _replaced ? _replacer : _curString;
                 _prevT = _curT;
             }
 
@@ -133,7 +159,7 @@ export function activate(context: ExtensionContext) {
     context.subscriptions.push(subOnDidChangedTextEditor);
     context.subscriptions.push(subOnDidChangeTextEditorSelection);
 
-   
+
 }
 
 
